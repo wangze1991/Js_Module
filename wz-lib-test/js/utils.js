c8e7634b83d98cdcd1a754b61289a95f1131f7f2
DEV-WANGZE\wangze <595702583@qq.com>
2017-04-24 15:25:23 +0800

    添加文件上传控件
    Signed-off-by: wangze <595702583@qq.com>

--- wz-lib-test/js/utils.js
+++ wz-lib-test/js/utils.js
@@ -1,15 +1,47 @@
 /**
  * Created by wangze on 2016/12/30.
  */
-;(function(){
+;(function () {
+    "use strict";
     var utils = {};
+
+
+    /**
+     * 关闭当前页面
+     */
+    function closeWebPage() {
+        if (navigator.userAgent.indexOf("MSIE") > 0) {//close IE
+            if (navigator.userAgent.indexOf("MSIE 6.0") > 0) {
+                window.opener = null;
+                window.close();
+            } else {
+                window.open('', '_top');
+                window.top.close();
+            }
+        }
+        else if (navigator.userAgent.indexOf("Firefox") > 0) {//close firefox
+            window.location.href = 'about:blank ';
+        } else {//close chrome;It is effective when it is only one.
+            window.opener = null;
+            window.open('', '_self');
+            window.close();
+        }
+    }
+
+    /**
+     * 获取浏览器类型
+     */
+    function explorerTest() {
+
+    }
+
 
     /**
      * 转换成jquery对象
      * @param obj jquery对象或jquery选择器
      * @returns {$object}
      */
-    utils.jquery= function(obj, $content){
+    utils.jquery = function (obj, $content) {
         var $obj = null;
         if (obj instanceof jQuery) {
             $obj = obj;
@@ -21,111 +53,126 @@
             }
         }
         return $obj;
-    }
+    };
+
     /**
      * 日期公共函数
+     * @param obj
+     * @param $content
+     * @returns {*}
      */
-    ;
-    (function(){
+
+    (function () {
         utils.dateUtil = {
-            format : function(time, fmt){
+            format: function (time, fmt) {
                 var o = {
-                    "M+" : time.getMonth() + 1,                 //月份
-                    "d+" : time.getDate(),                    //日
-                    "h+" : time.getHours(),                   //小时
-                    "m+" : time.getMinutes(),                 //分
-                    "s+" : time.getSeconds(),                 //秒
-                    "q+" : Math.floor((time.getMonth() + 3) / 3), //季度
-                    "S" : time.getMilliseconds()             //毫秒
+                    "M+": time.getMonth() + 1, //月份
+                    "d+": time.getDate(), //日
+                    "h+": time.getHours(), //小时
+                    "m+": time.getMinutes(), //分
+                    "s+": time.getSeconds(), //秒
+                    "q+": Math.floor((time.getMonth() + 3) / 3), //季度
+                    "S": time.getMilliseconds() //毫秒
                 };
-                if(/(y+)/.test(fmt))
+                if (/(y+)/.test(fmt))
                     fmt = fmt.replace(RegExp.$1, (time.getFullYear() + "").substr(4 - RegExp.$1.length));
-                for(var k in o)
-                    if(new RegExp("(" + k + ")").test(fmt))
+                for (var k in o)
+                    if (new RegExp("(" + k + ")").test(fmt))
                         fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
                 return fmt;
             }
         };
     })();
 
-
     /**
      * 字符串公共方法
      */
-    ;
-    (function(){
+    (function () {
         utils.stringUtil = {
             /**字符串格式化
              @param str 字符串
              @param value 替换值
              @example  "我是{0},身高{1}"
              */
-            'format' : function(str, value){
-                if(!value || !str) return "";
-                for(var i = 0; i < arguments.length; i++){
-                    var j = i + 1;
-                    if(arguments[j] != undefined){
-                        var reg = new RegExp("({)" + i + "(})", "g");
-                        str = str.replace(reg, arguments[j]);
+            'format': function (str, value) {
+                if (utils.isn)
+                    for (var i = 0; i < arguments.length; i++) {
+                        var j = i + 1;
+                        if (arguments[j] != undefined) {
+                            var reg = new RegExp("({)" + i + "(})", "g");
+                            str = str.replace(reg, arguments[j]);
+                        }
                     }
-                }
                 return str;
-            }
-            ,'trimEnd' : function(str){
-                if(str.length == 0) return str;
-                if(str.lastIndexOf(str) == str.length - 1){
-                    return this.substr(0, str.length - 1);
+            },
+            'trim': function (s) {
+                // return $.trim(str);
+                var str = "" + s,
+                    ws = /\s/,
+                    i = str.length;
+                str = str.replace(/^\s\s*/, "");
+                while (ws.test(str.charAt(--i))) {};
+                return str.slice(0, i + 1);
+            },
+            'isBlank': function (str) {
+                return !utils.isNotBlank(str);
+            },
+            'isNotBlank': function (str) {
+                if (str === undefined || str === null || str === '') {
+                    return false;
                 }
-                return this;
-            }
-            ,'trim' : function(str){
-                return $.trim(str);
+                return $.trim(str).length > 0;
             }
         };
     })();
 
-
     /**
      * 表单公共方法
      */
-    ;(function(){
+    (function () {
         utils.formUtil = {
             /**
-             *绑定表单
+             * 绑定表单
              * @param form
              * @param param
              */
-            'bindForm' : function(form, param){
+            'bindForm': function (form, param) {
                 var $form = $(form);
-                if (!param) return;
+                if (!param) {
+                    return;
+                }
                 $('input', $form).each(function () {
-                    if (param.hasOwnProperty(this.name) === false)return true;
+                    if (param.hasOwnProperty(this.name) === false) {
+                        return true;
+                    }
                     var val = param[this.name];
                     //如果是数组，这里直接不判断,单独写方法重构，遇到具体情况在分析。
-                    if ($.isArray(val))return true;
-                    if (typeof val === 'object') return true;
+                    if ($.isArray(val)) {
+                        return true;
+                    }
+                    if (typeof val === 'object') {
+                        return true;
+                    }
                     if (this.type == "text" || this.type == "hidden") {
                         $(this).val(val);
-                    }
-                    else if (this.type == "checkbox") {
+                    } else if (this.type == "checkbox") {
                         $(this).prop("checked", val == "True" || val == true);
-                    }
-                    else if ((this.type == "radio")) {
+                    } else if ((this.type == "radio")) {
                         $("input[name=" + this.name + "][value=" + val + "]").prop("checked", true);
                     }
                 });
                 $('textarea,select', $form).each(function () {
-                    if (param.hasOwnProperty(this.name) === false)return true;
+                    if (param.hasOwnProperty(this.name) === false) {
+                        return true
+                    }
+                    ;
                     $(this).val(param[this.name]);
                 });
             }
             /**
-             * 表单值转换为[{name:'',value}]键值对
-             * @param $form
-             * @returns {Array}
-             * tips radiobutton 没有考虑
+             * 表单值转换为[{name:'',value}]键值对,也可以直接使用jquery.serialize
              */
-            ,'formToArray' : function($form){
+            , 'formToArray': function ($form) {
                 var a = [];
                 if ($form.length === 0) {
                     return a;
@@ -142,8 +189,12 @@
                     if (!n) {
                         continue;
                     }
-                    if(el.type=='checkbox'){
-                        if($(el).prop("checked")!==true)
+                    if (el.type == 'checkbox') {
+                        if ($(el).prop("checked") !== true)
+                            continue;
+                    }
+                    if (el.type == 'radio') {
+                        if ($(el).prop("checked") !== true)
                             continue;
                     }
                     v = $.trim($(el).val());
@@ -151,12 +202,13 @@
                         for (j = 0, jmax = v.length; j < jmax; j++) {
                             a.push({name: n, value: v[j]});
                         }
-                    }
-                    else if (v !== null && typeof v != 'undefined' && v != '') {
+                    } else if (v !== null && typeof v != 'undefined' && v != '') {
                         a.push({name: n, value: v});
                     }
                 }
                 return a;
+            },'formToObject': function () {
+
             }
         };
     })();
